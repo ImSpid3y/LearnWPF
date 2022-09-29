79246587a78b3f185f4fc3c96f605b4f4b754144
ImSpid3y <im.souravpradhan@outlook.com>
2022-09-29 17:45:33 +0530

    Add project files.

---
+++ LearnWPF/ViewModels/YoutubeViewersListingViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWPF.ViewModels
+{
+    public class YoutubeViewersListingViewModel : ViewModelBase
+    {
+        private readonly ObservableCollection<YoutubeViewersListingItemViewModel> _youtubeViewersListingItemsViewModels;
+
+        IEnumerable<YoutubeViewersListingItemViewModel> YoutubeViewersListingItemViewModels => _youtubeViewersListingItemsViewModels;
+
+        public YoutubeViewersListingViewModel()
+        {
+            _youtubeViewersListingItemsViewModels = new ObservableCollection<YoutubeViewersListingItemViewModel>();
+            _youtubeViewersListingItemsViewModels.Add(new YoutubeViewersListingItemViewModel("Sourav"));
+            _youtubeViewersListingItemsViewModels.Add(new YoutubeViewersListingItemViewModel("Srikanta"));
+            _youtubeViewersListingItemsViewModels.Add(new YoutubeViewersListingItemViewModel("Sambhu"));
+        }
+    }
+}
